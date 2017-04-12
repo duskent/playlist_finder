bd562bf4d13c9e74cd68ffcf52e2e9adba35323c
Julian Ronayi <duskent2@gmail.com>
2017-04-12 17:41:31 +0300

    Add form to main page

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import {Grid, PageHeader} from 'react-bootstrap'
+import {Grid, Button, Col, Well, PageHeader, FormGroup, ControlLabel, FormControl} from 'react-bootstrap'
+import Textarea from 'react-textarea-autosize'
 import 'bootstrap/dist/css/bootstrap.css'
 import './main.scss'
 
@@ -7,9 +8,28 @@ class App extends Component {
   render() {
     return (
       <Grid>
-        <PageHeader onClick={this.props.clickHeader}>
-          Hello World
-        </PageHeader>
+        <Well>
+          <PageHeader className="center" onClick={this.props.clickHeader}>
+            PlaylstFinder - Find songs from you playlists!
+          </PageHeader>
+          <h2 className="center">
+            <small>Just insert text into box below</small>
+          </h2>
+        </Well>
+        <Well>
+          <Grid>
+            <Col xs={12} md={8} mdOffset={2}>
+              <form>
+                <div className="form-group">
+                  <Textarea className="form-control" rows="5" />
+                </div>
+              </form>
+              <Button className="btn btn-primary" id="search-button" type="submit">
+                Search Playlist
+              </Button>
+            </Col>
+          </Grid>
+        </Well>
       </Grid>
     )
   }
